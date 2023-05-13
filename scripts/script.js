01cddc4e1f0a57267516cddecbaa667dd3921298
maksim_bogatikov <maxim.bogatikov@mail.ru>
2023-05-13 19:44:33 +0300

    remove function and fixes

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,38 @@ const list = document.querySelector('.list');
 const textBox = document.querySelector('.text-box');
 const tasks = document.querySelectorAll('.task');
 
+const task = document.createElement("div");
+task.classList.add('task');
+const text = document.createElement("div");
+const deleteButton = document.createElement("div");
+task.append(text);
+
 addButton.addEventListener('click', () => {
     const text = textBox.value;
     if (text !== "") {
-        const task = document.createElement("div");
-        task.innerText = textBox.value;
-        task.classList.add('task');
-        task.addEventListener('click', () => {task.classList.toggle('done');});
-        list.insertAdjacentElement('beforeend', task);
+        list.insertAdjacentElement('beforeend', createTask(textBox.value));
         textBox.value = null;
     }
 });
+
+function createTask(text){
+    const task = document.createElement("div");
+    task.classList.add('task');
+    const textContainer = document.createElement("div");
+    textContainer.classList.add('task-text');
+    textContainer.innerText = text;
+    const deleteButton = document.createElement("div");
+    deleteButton.innerText = 'Delete';
+    deleteButton.classList.add('delete-button');
+    deleteButton.classList.add('none');
+    task.addEventListener('mouseover', () => {deleteButton.classList.toggle('none')});
+    task.addEventListener('mouseout', () => {deleteButton.classList.toggle('none')});
+    deleteButton.addEventListener('click', () => {task.remove()});
+    task.addEventListener('click', () => {
+        task.classList.toggle('green-border');
+        textContainer.classList.toggle('line-through');
+    });
+    task.append(textContainer, deleteButton);
+
+    return task;
+}
